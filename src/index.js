9dc098f63c3a4e86ffa41e5b86aa795aefa8ff07
David Weiland <weiland.david@icloud.com>
2022-02-24 18:30:03 +0100

    init routes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import AdminApp from './Pages/AdminApp';
+import Error from './Pages/Error'
+import { Provider } from 'react-redux'
+import store from './Utils/store'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/admin' element={<AdminApp />} />
+          <Route path='*' element={<Error />} />
+        </Routes>
+      </Router>
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
