4da1d15457c59aa4eed26930a6cc036668b72a2b
David Weiland <weiland.david@icloud.com>
2022-03-01 19:11:33 +0100

    login

--- src/Pages/AdminApp/index.jsx
+++ src/Pages/AdminApp/index.jsx
@@ -1,12 +1,12 @@
 import React, {useState} from "react";
 import { useSelector, useStore } from 'react-redux'
 import { selectAdmin } from '../../Utils/selectors'
-import { signupAdmin, loginAdmin, modifyAdmin, resetAdmin } from '../../Features/admin'
+import { signupAdmin, modifyAdmin } from '../../Features/admin'
 
 export default function AdminApp() {
   const store = useStore()
-  const [ identifiant, setIdentifiant ] = useState("David")
-  const [ password, setPassword ] = useState("123")
+  const [ identifiant, setIdentifiant ] = useState("")
+  const [ password, setPassword ] = useState("")
   
   const admin = useSelector(selectAdmin)
   const adminId = admin.data?.adminId
@@ -25,38 +25,21 @@ export default function AdminApp() {
     modifyAdmin(store, body, adminId, token)
   }
 
-  const login = () => {
-    loginAdmin(store, body)
-  }
-  
-  const reset = () => {
-    resetAdmin(store)
-  }
-
-  const connected = (
-    <div>
-      {admin.data?.name !== identifiant ?
-        <button onClick={signup}>Nouveau</button> :
-        <div><button onClick={modify}>Modifier</button>
-          <button onClick={reset}>Se déconnecter</button></div>}
-    </div>)
-
   return (
     <div>
-      <h1>Bonjour {admin.data?.name},</h1>
+      <span>
+        Pour créer un nouvel identifiant, vous devez vous rapproché d'un administrateur déjà enregistré. Celui-ci devra s'identifier pour créer le nouveau compte.
+      </span>
       <form>
         <label htmlFor="Identifiant"> Identifiant :</label>
         <input type='text' id='Identifiant' name='Identifiant' value={identifiant} onChange={(e) => setIdentifiant(e.target.value)} />
         <label htmlFor="Password"> Password :</label>
         <input type='text' id='Password' name='Password' value={password} onChange={(e)=>setPassword(e.target.value)}/>
       </form>
-      <span>
-        Pour créer un nouvel identifiant, vous devez vous rapproché d'un administrateur déjà enregistré. Celui-ci devra s'identifier pour créer le nouveau compte.
-      </span>
-      
-      <br/><br/>
-      {(adminId) ? connected :
-        <button onClick={login}>Se connecter</button>}
+      {admin.data?.name !== identifiant ?
+        <button onClick={signup}>Nouveau</button> :
+        <button onClick={modify}>Modifier</button>
+      }
     </div>
   );
 }
