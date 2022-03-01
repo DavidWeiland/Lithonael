6fd83d74b65f910b8ba1a328afdc182b77c89b31
David Weiland <weiland.david@icloud.com>
2022-03-01 11:21:43 +0100

    add general style

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './Utils/Styles/style.css'
+import Header from './Components/Header';
 import Home from './Pages/Home';
 import AdminApp from './Pages/AdminApp';
+import GemsList from './Pages/gemsList';
+import GemCreator from './Pages/GemCreator';
 import Error from './Pages/Error'
 import { Provider } from 'react-redux'
 import store from './Utils/store'
@@ -11,9 +15,12 @@ ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
       <Router>
+        <Header/>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/wikigems' element={<GemsList />} />
           <Route path='/admin' element={<AdminApp />} />
+          <Route path='/admin/gemcreator' element={<GemCreator />} />
           <Route path='*' element={<Error />} />
         </Routes>
       </Router>
