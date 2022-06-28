fac7936345580f3e6c41a459679ff65db5fce983
David Weiland <weiland.david@icloud.com>
2022-06-28 18:40:35 +0200

    styles/colors

---
+++ src/Utils/Styles/colors.js
@@ -0,0 +1,14 @@
+const colors = {
+  primary: '#FCF8E8',
+  secondary: '#94B49F',
+  tertiary:'#ECB390',
+  quaternary: '#DF7861',
+  
+  fontDark: '#000',
+  fontLight: '#FCF8E8',
+
+  boxShadowDark:'5px 5px 10px rgba(0, 0, 0, 0.25)',
+  boxShadowLight:'5px 5px 10px rgba(255, 255, 255, 0.25)',
+}
+
+export default colors
