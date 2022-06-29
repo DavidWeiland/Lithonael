200e8bed0da3047b10982c6aa3114428103fd173
David Weiland <weiland.david@icloud.com>
2022-06-29 05:36:12 +0200

    universalize button

--- src/Components/Button/index.jsx
+++ src/Components/Button/index.jsx
@@ -2,20 +2,16 @@ import React from 'react'
 import styled from 'styled-components'
 import colors from '../../Utils/Styles/colors'
 
-export const Button = ({action, children}) => {
+export const Button = ({style, action, children}) => {
 
   return (
-    <StyledButton onClick={action}>{children}</StyledButton>
+    <StyledButton style={style} onClick={action}>{children}</StyledButton>
   )
 }
 
 const StyledButton = styled.button`
-  margin-top: 50px;
-  font-family: 'Alegreya', serif;
-  font-style: normal;
   font-weight: 400;
   font-size: 20px;
-  line-height: 27px;
   display: flex;
   align-items: center;
   justify-content:center;
